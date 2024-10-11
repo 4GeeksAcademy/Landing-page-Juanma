597a62067a7165a5e0672491394547b8e96562f8
Bertitom <juanbertoletti2@gmail.com>
2024-10-11 22:18:57 +0000

    new commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,14 +8,33 @@ import Card from "./Card";
 
 //create your first component
 const Home = () => {
-	return (
-		<>
-			<Navbar/>
-			<Jumbotron content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ullam id quae adipisci. Ullam quam quibusdam delectus? Necessitatibus dignissimos obcaecati quod cum, repellendus'/>
-			<Card title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-			<Footer/>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Jumbotron content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ullam id quae adipisci. Ullam quam quibusdam delectus? Necessitatibus dignissimos obcaecati quod cum, repellendus" />
+      <div className="container text-center mt-5">
+        <div className="row">
+          <Card
+            title="Card title"
+            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+          />
+          <Card
+            title="Card title"
+            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+          />
+          <Card
+            title="Card title"
+            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+          />
+          <Card
+            title="Card title"
+            text="Some quick example text to build on the card title and make up the bulk of the card's content."
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
